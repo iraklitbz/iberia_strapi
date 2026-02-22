b2270a9e298c3a1dfb7bb030a2547401f60f51dd
irakli <iraklitbz@gmail.com>
2026-02-22 11:59:20 +0100

    added roles

--- src/api/entrada/controllers/entrada.ts
+++ src/api/entrada/controllers/entrada.ts
@@ -1,2 +1,35 @@
-import { factories } from '@strapi/strapi';
-export default factories.createCoreController('api::entrada.entrada');
+import { factories } from '@strapi/strapi'
+
+function getAcceso(user: any): string[] {
+  const levels = ['publico']
+  if (!user) return levels
+  levels.push('miembros')
+  if (user.role?.name === 'Suscriptor') levels.push('suscriptores')
+  return levels
+}
+
+export default factories.createCoreController('api::entrada.entrada', ({ strapi }) => ({
+  async find(ctx) {
+    const allowed = getAcceso(ctx.state.user)
+    ctx.query = {
+      ...ctx.query,
+      filters: {
+        ...(ctx.query.filters as object ?? {}),
+        $or: [
+          { acceso: { $in: allowed } },
+          { acceso: { $null: true } },
+        ],
+      },
+    }
+    return super.find(ctx)
+  },
+
+  async findOne(ctx) {
+    const response = await super.findOne(ctx)
+    if (!response?.data) return response
+    const allowed = getAcceso(ctx.state.user)
+    const nivel = response.data.acceso ?? 'publico'
+    if (!allowed.includes(nivel)) return ctx.forbidden('Acceso restringido')
+    return response
+  },
+}))
